f5803d33211cc4dfb0dad61006e1d8c0aa1754ef
kodolic <53709581+kodolic@users.noreply.github.com>
2023-09-05 17:22:15 +0300

    repoort 5.9

--- slam/include/Converter.h
+++ slam/include/Converter.h
@@ -23,34 +23,39 @@
 
 #include<opencv2/core/core.hpp>
 
-#include<Eigen/Dense>
-#include"g2o/types/types_six_dof_expmap.h"
-#include"g2o/types/types_seven_dof_expmap.h"
+#include<eigen3/Eigen/Dense>
+#include"../Thirdparty/g2o/g2o/types/types_six_dof_expmap.h"
+#include"../Thirdparty/g2o/g2o/types/types_seven_dof_expmap.h"
 
 namespace ORB_SLAM2
 {
 
-class Converter
-{
-public:
-    static std::vector<cv::Mat> toDescriptorVector(const cv::Mat &Descriptors);
+    class Converter
+    {
+    public:
+        static std::vector<cv::Mat> toDescriptorVector(const cv::Mat& Descriptors);
 
-    static g2o::SE3Quat toSE3Quat(const cv::Mat &cvT);
-    static g2o::SE3Quat toSE3Quat(const g2o::Sim3 &gSim3);
+        static g2o::SE3Quat toSE3Quat(const cv::Mat& cvT);
+        static g2o::SE3Quat toSE3Quat(const g2o::Sim3& gSim3);
 
-    static cv::Mat toCvMat(const g2o::SE3Quat &SE3);
-    static cv::Mat toCvMat(const g2o::Sim3 &Sim3);
-    static cv::Mat toCvMat(const Eigen::Matrix<double,4,4> &m);
-    static cv::Mat toCvMat(const Eigen::Matrix3d &m);
-    static cv::Mat toCvMat(const Eigen::Matrix<double,3,1> &m);
-    static cv::Mat toCvSE3(const Eigen::Matrix<double,3,3> &R, const Eigen::Matrix<double,3,1> &t);
+        static cv::Mat toCvMat(const g2o::SE3Quat& SE3);
+        static cv::Mat toCvMat(const g2o::Sim3& Sim3);
+        static cv::Mat toCvMat(const Eigen::Matrix<double, 4, 4>& m);
+        static cv::Mat  Converter::toCvMatf(const Eigen::Matrix<float, 4, 4>& m);
+        static cv::Mat toCvMat(const Eigen::Matrix3d& m);
+        static cv::Mat toCvMat(const Eigen::Matrix<double, 3, 1>& m);
+        static cv::Mat toCvSE3(const Eigen::Matrix<double, 3, 3>& R, const Eigen::Matrix<double, 3, 1>& t);
 
-    static Eigen::Matrix<double,3,1> toVector3d(const cv::Mat &cvVector);
-    static Eigen::Matrix<double,3,1> toVector3d(const cv::Point3f &cvPoint);
-    static Eigen::Matrix<double,3,3> toMatrix3d(const cv::Mat &cvMat3);
+        static Eigen::Matrix<double, 3, 1> toVector3d(const cv::Mat& cvVector);
+        static Eigen::Matrix<double, 3, 1> toVector3d(const cv::Point3f& cvPoint);
+        static Eigen::Matrix<double, 3, 3> toMatrix3d(const cv::Mat& cvMat3);
 
-    static std::vector<float> toQuaternion(const cv::Mat &M);
-};
+        static Eigen::Matrix<float, 3, 1> Converter::toVector3f(const cv::Mat& cvVector);
+        static Eigen::Matrix<float, 3, 3> Converter::toMatrix3f(const cv::Mat& cvMat3);
+        static Eigen::Vector4f Converter::toVector4f(const cv::Mat& cvVector);
+
+        static std::vector<float> toQuaternion(const cv::Mat& M);
+    };
 
 }// namespace ORB_SLAM
 
